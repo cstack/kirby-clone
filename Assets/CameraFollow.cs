e58b3115cc1cf6ec3474bcd2f17e1469bdb5b96b
Connor Stack <cstack@umich.edu>
2014-01-15 17:55:00 -0500

    Kirby character and basic controls.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform poi;
+	public Vector3 offset = new Vector3(0,0,-10);
+	public float u = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 target = poi.position + offset;
+		Vector3 currentPos = transform.position;
+		transform.position = (1 - u) * currentPos + u * target;
+	}
+}
---
+++ Assets/KirbyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KirbyScript : MonoBehaviour {
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		Vector2 vel = rigidbody2D.velocity;
+		vel.x = h * speed;
+		rigidbody2D.velocity = vel;
+	}
+}
