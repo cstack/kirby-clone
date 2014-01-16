4c8811fd47eec4908fedbdaf520be3f5478fffc0
Gulshan Singh <gulshan@umich.edu>
2014-01-15 20:06:06 -0800

    Added basic jump mechanics, and added infrastructure for other mechanics

--- Assets/Scripts/Kirby.cs
+++ Assets/Scripts/Kirby.cs
@@ -3,17 +3,61 @@ using System.Collections;
 
 public class Kirby : MonoBehaviour {
 	public float speed = 10f;
+	public float jumpSpeed = 10f;
+	
+	/*
+	 * All Vertical/Inhale state combinations are valid except for
+	 * Flying/Inhaling and Flying/Inhaled
+	 */
+	private VerticalState verticalState = VerticalState.JUMPING;
+	private InhaleState inhaleState = InhaleState.NOT_INHALING;
 
-	// Use this for initialization
-	void Start () {
-	
+	private enum VerticalState {
+		GROUND, JUMPING, FLYING
 	}
-	
-	// Update is called once per frame
+	private enum InhaleState {
+		NOT_INHALING, INHALING, INHALED
+	}
+
 	void Update () {
-		float h = Input.GetAxis ("Horizontal");
+		handleInhaling(); // This must come before other handlers
+		handleHorizontalMovement();
+		handleJumping();
+		handleFlying();
+	}
+
+	void handleInhaling() {
+		// TODO
+	}
+
+	void handleHorizontalMovement() {
+		if (inhaleState == InhaleState.INHALING) {
+			return;
+		}
+
+		float h = Input.GetAxis("Horizontal");
 		Vector2 vel = rigidbody2D.velocity;
 		vel.x = h * speed;
 		rigidbody2D.velocity = vel;
+	}
+
+	void handleJumping() {
+		if (inhaleState == InhaleState.INHALING) {
+			return;
+		}
+
+		Vector2 vel = rigidbody2D.velocity;
+		if (Input.GetKey(KeyCode.X)) {
+			vel.y = jumpSpeed;
+		}
+		rigidbody2D.velocity = vel;
+	}
+
+	void handleFlying() {
+		if (inhaleState == InhaleState.INHALING
+		    || inhaleState == InhaleState.INHALED) {
+			return;
+		}
+		// TODO
 	}
 }
