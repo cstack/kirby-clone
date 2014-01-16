1c349fed81b0078be3cd6db51cd46ca864647832
Connor Stack <cstack@umich.edu>
2014-01-16 00:43:43 -0500

    Don't let Kirby or the camera go outside the bounds of the level.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,18 +3,30 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public Transform poi;
-	public Vector3 offset = new Vector3(0,0,-10);
 	public float u = 0.4f;
+	public float distanceFromEdge = 6.6f;
+	public float levelWidth = 63.7f;
+	public float y = 27.6f;
+	public float z = -9f;
 
 	// Use this for initialization
 	void Start () {
-	
+		poi = GameObject.Find ("Kirby").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 target = poi.position + offset;
-		Vector3 currentPos = transform.position;
-		transform.position = (1 - u) * currentPos + u * target;
+		float target = poi.position.x;
+
+		// Don't go past edge of level
+		if (target < distanceFromEdge) {
+			target = distanceFromEdge;
+		} if (target > (levelWidth - distanceFromEdge)) {
+			target = (levelWidth - distanceFromEdge);
+		}
+
+		float currentX = transform.position.x;
+		float newX = (1 - u) * currentX + u * target;
+		transform.position = new Vector3 (newX, y, z);
 	}
 }
