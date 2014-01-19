5856a61fb8d8771ba100c3268cfd4da735c322c9
Gulshan Singh <gulshan@umich.edu>
2014-01-18 16:33:05 -0800

    Added AnimationManager

---
+++ Assets/Scripts/Lib/AnimationManager.cs
@@ -0,0 +1,51 @@
+﻿using AnimationEnums;
+using System.Collections;
+using UnityEngine;
+
+namespace AnimationEnums {
+	public enum IdleOrWalking {
+		IDLE, WALKING
+	}
+	
+	public enum Direction {
+		LEFT, RIGHT
+	}
+}
+
+public class AnimationManager {
+
+	private Animator animator;
+
+	private Direction dir;
+	private int state;
+
+	public AnimationManager(Animator animator) {
+		this.animator = animator;
+		Dir = Direction.RIGHT;
+	}
+
+	public Direction Dir {
+		get {
+			return dir;
+		}
+		set {
+			dir = value;
+			animator.SetInteger("Direction", (int) dir);
+		}
+	}
+
+	public int State {
+		get {
+			return state;
+		}
+		set {
+			state = value;
+			animator.SetInteger("State", value);
+			animator.SetInteger("SubState", 0);
+		}
+	}
+
+	public void animate(int subState) {
+		animator.SetInteger("SubState", subState);
+	}
+}
