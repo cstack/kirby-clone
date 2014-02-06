748b21728d7bfe185bfec4cc67f71ef0b08c90e5
Gulshan Singh <gulshan@umich.edu>
2014-02-06 16:16:21 -0500

    Tweaked more WaddleDoo parameters

--- Assets/Scripts/Character/WaddleDoo.cs
+++ Assets/Scripts/Character/WaddleDoo.cs
@@ -40,7 +40,7 @@ public class WaddleDoo : EnemyBase {
 
 	IEnumerator ChargeEnterState() {
 		updateXVelocity (0f);
-		yield return new WaitForSeconds(2f);
+		yield return new WaitForSeconds(1.5f);
 		CurrentState = State.Attack;
 	}
 
