a0a15f0b384c745e20739a68cb13a5aa02a1a1c7
Connor Stack <cstack@umich.edu>
2014-02-07 17:07:18 -0500

    HotHead states.

--- Assets/Scripts/Character/HotHead.cs
+++ Assets/Scripts/Character/HotHead.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using AnimationEnums;
+﻿using UnityEngine;
+using System.Collections;
 
 public class HotHead : EnemyBase {
 	public float speed = 3f;
 	public float range = 3f;
-	public float timeBetweenAttacks = 3f;
+	public float timeBetweenAttacks = 2.5f;
 	
 	private enum State {
 		Walk, Prepare, Flamethrower, Fireball
@@ -16,11 +16,35 @@ public class HotHead : EnemyBase {
 
 	#region Walk
 
+	public IEnumerator WalkEnterState() {
+		yield return new WaitForSeconds(timeBetweenAttacks);
+		if (CurrentState.ToString() == State.Walk.ToString()) {
+			CurrentState = State.Prepare;
+		}
+	}
+
 	public void WalkUpdate() {
 		updateXVelocity(speed * (dir == Direction.Right ? 1 : -1));
 		BounceOffWalls();
 	}
 
+	#endregion
+
+	#region Prepare
+	
+	public IEnumerator PrepareEnterState() {
+		Vector3 towardKirby = kirby.transform.position - transform.position;
+		if ((towardKirby.x > 0 && dir == Direction.Left) ||
+		    (towardKirby.x < 0 && dir == Direction.Right)) {
+			Flip();
+		}
+		updateXVelocity(0f);
+		yield return new WaitForSeconds(0.5f);
+		if (CurrentState.ToString() == State.Prepare.ToString()) {
+			CurrentState = State.Flamethrower;
+		}
+	}
+	
 	#endregion
 
 	#region implemented abstract members of EnemyBase
