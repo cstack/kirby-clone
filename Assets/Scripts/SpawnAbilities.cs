3750e070fc0adc74045351c6558e7054402757ff
Gulshan Singh <gulshan@umich.edu>
2014-02-11 17:12:29 -0500

    Added randomly generated ability stars for boss battle

---
+++ Assets/Scripts/SpawnAbilities.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAbilities : MonoBehaviour {
+
+	public AbilityStar starPrefab;
+
+	IEnumerator Start() {
+		while (true) {
+			AbilityStar star = Instantiate(starPrefab) as AbilityStar;
+			Vector3 pos = star.transform.position;
+			pos.x = camera.pixelWidth / 16 * Random.value;
+			pos.y = camera.pixelHeight / 16;
+			pos.z = -0.1f	;
+			star.transform.position = pos;
+			Debug.Log(pos);
+			yield return new WaitForSeconds(3f);
+		}
+	}
+}
