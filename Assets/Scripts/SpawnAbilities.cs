917fe9af30a5891df38e7868dbfabc174d71bdba
Connor Stack <cstack@umich.edu>
2014-02-11 23:54:07 -0500

    Spawn stars at 3 random location.

--- Assets/Scripts/SpawnAbilities.cs
+++ Assets/Scripts/SpawnAbilities.cs
@@ -8,21 +8,23 @@ public class SpawnAbilities : MonoBehaviour {
 	IEnumerator Start() {
 		while (true) {
 			AbilityStar star = Instantiate(starPrefab) as AbilityStar;
-			int val = Random.Range(0, 2);
+			star.transform.position = transform.position - new Vector3(0f, 0f, transform.position.z);
+			int val = Random.Range(0, 3);
+			Debug.Log("Random star " + val);
 			if (val == 0) {
 				star.ability = star.energyWhip;
 				star.abilityCard = star.energyWhipCard;
+				star.transform.position += new Vector3(0f, 5f, 0f);
 			} else if (val == 1) {
 				star.ability = star.spark;
 				star.abilityCard = star.sparkCard;
+				star.transform.position += new Vector3(-3f, 3f, 0f);
 			} else if (val == 2) {
 				star.ability = star.flameThrower;
 				star.abilityCard = star.flameThrowerCard;
+				star.transform.position += new Vector3(3f, 3f, 0f);
 			}
-			star.transform.position = transform.position - new Vector3(0f, 0f, transform.position.z);
-			float xOffset = camera.pixelWidth / 16 * (Random.value - 0.5f);
-			star.transform.position += new Vector3(xOffset, 0f, 0f);
-			star.transform.position += new Vector3(0f, 3f, 0f);
+
 			yield return new WaitForSeconds(3f);
 		}
 	}
